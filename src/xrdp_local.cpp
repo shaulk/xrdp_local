22bf19b3f18d4f0bfe231953439d4206cabf5fea
Shaul Kremer <shaulk@users.noreply.github.com>
2025-08-03 19:51:46 +0300

    Use libargparse for command line arugments.

--- src/xrdp_local.cpp
+++ src/xrdp_local.cpp
@@ -1,14 +1,15 @@
 #include <sys/select.h>
 #include <unistd.h>
+#include <argparse/argparse.hpp>
 
 #include "common.h"
 #include "xrdp_local.h"
 #include "xup.h"
 #include "qt.h"
 
-XRDPLocalState::XRDPLocalState(const char *socket_path, int feedback_fd) {
+XRDPLocalState::XRDPLocalState(const char *socket_path, int feedback_fd, int max_displays) {
 	this->feedback_fd = feedback_fd;
-	qt = new QtState(this, 1);
+	qt = new QtState(this, max_displays);
 	xup = new XRDPModState(this, qt, socket_path);
 	notify_feedback_fd("connected");
 	qt->launch();
@@ -38,12 +39,36 @@ void XRDPLocalState::notify_feedback_fd(const char *msg) {
 }
 
 int main(int argc, char *argv[]) {
-	if (argc != 3) {
-		fprintf(stderr, "Usage: %s <socket_path> <feedback_fd>\n", argv[0]);
+	argparse::ArgumentParser program("xrdp_local");
+
+	program.add_argument("-v", "--verbose")
+		.help("verbose output")
+		.default_value(false)
+		.implicit_value(true);
+
+	program.add_argument("socket-path")
+		.help("set the socket path");
+
+	program.add_argument("feedback-fd")
+		.help("set the feedback fd")
+		.default_value(-1)
+		.scan<'i', int>();
+
+	program.add_argument("--max-displays")
+		.help("set the maximum number of displays")
+		.default_value(0)
+		.scan<'i', int>();
+
+	try {
+		program.parse_args(argc, argv);
+	} catch (const std::runtime_error& err) {
+		fprintf(stderr, "%s\n", err.what());
 		return 1;
 	}
 
-	XRDPLocalState state(argv[1], atoi(argv[2]));
+	set_log_level(program.get<bool>("-v") ? LOG_DEBUG : LOG_INFO);
+
+	XRDPLocalState state(program.get<std::string>("socket-path").c_str(), program.get<int>("feedback-fd"), program.get<int>("--max-displays"));
 
 	state.qt->run();
 
